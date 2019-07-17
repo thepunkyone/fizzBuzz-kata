d875e1dbd934662da1077e169f6f3800d5ef382e
Vitnija Bluzma <thepunkyone@gmail.com>
2019-07-17 11:29:10 +0100

    fourth failing test

--- __tests__/fizzbuzz.test.js
+++ __tests__/fizzbuzz.test.js
@@ -2,9 +2,13 @@ const fizzBuzz = require('../fizzbuzz')
 
 describe('fizzBuzz', () => {
 
-    it('returns a string value of the number parameter', () => {
+    it('returns a string value of the number supplied', () => {
         expect(fizzBuzz(1)).toBe('1')
         expect(fizzBuzz(2)).toBe('2')
         expect(fizzBuzz(4)).toBe('4')
+    })
+
+    it('returns a string Fizz for the number 3', () => {
+        expect(fizzBuzz(3)).toBe('Fizz')
     })
 })
