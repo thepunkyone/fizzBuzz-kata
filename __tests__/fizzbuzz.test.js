e0b36aa0193adbe6c5ae9a15509fdab5c3ee6580
Vitnija Bluzma <thepunkyone@gmail.com>
2019-07-17 10:55:13 +0100

    second test

--- __tests__/fizzbuzz.test.js
+++ __tests__/fizzbuzz.test.js
@@ -1,7 +1,12 @@
 const fizzBuzz = require('../fizzbuzz')
 
 describe('fizzBuzz', () => {
+
     it('returns a string 1 for number 1', () => {
-        expect(fizzBuzz(1)).toBe('1');
+        expect(fizzBuzz(1)).toBe('1')
+    })
+
+    it('returns a string 2 for number 2', () => {
+        expect(fizzBuzz(2)).toBe('2')
     })
 })
