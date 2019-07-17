d735643c10a0e41fd1dc8b388679a58f55afaec6
Vitnija Bluzma <thepunkyone@gmail.com>
2019-07-17 11:45:16 +0100

    test passing

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
 const fizzBuzz = (number) => {
-    if (number === 5) return 'Buzz'
+    if (number === 5 || number === 10) return 'Buzz'
     if (number % 3 === 0) return 'Fizz'
     return number.toString()
 }
