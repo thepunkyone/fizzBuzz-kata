dfc1fe0eb1087c18ed96ac9b1efe395de8cc710d
Vitnija Bluzma <thepunkyone@gmail.com>
2019-07-17 11:48:05 +0100

    refactored function

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
 const fizzBuzz = (number) => {
-    if (number === 5 || number === 10 || number === 40) return 'Buzz'
+    if (number % 5 === 0) return 'Buzz'
     if (number % 3 === 0) return 'Fizz'
     return number.toString()
 }
